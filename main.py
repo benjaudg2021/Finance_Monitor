d90daad095e9baa7c49628a2d4aef6a7ceae6658
benjaudg2021 <benjaudg2021@gmail.com>
2026-04-21 22:07:52 -0600

    develp try 2 branch nueva

--- main.py
+++ main.py
@@ -31,3 +31,5 @@ def monitorear_accion(ticket_simbolo):
 if __name__ == "__main__":
     # Puedes cambiar 'MSTR' por cualquier otro ticker como 'TSLA' o 'AAPL'
     monitorear_accion("MSTR")
+
+    #develop try 2 comment
