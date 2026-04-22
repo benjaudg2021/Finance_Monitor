20980fc9add5957b9baf04e36c241e2e1578021e
benjaudg2021 <benjaudg2021@gmail.com>
2026-04-21 21:53:20 -0600

    1st commint in branch try1

--- main.py
+++ main.py
@@ -31,3 +31,4 @@ def monitorear_accion(ticket_simbolo):
 if __name__ == "__main__":
     # Puedes cambiar 'MSTR' por cualquier otro ticker como 'TSLA' o 'AAPL'
     monitorear_accion("MSTR")
+    #change 1
